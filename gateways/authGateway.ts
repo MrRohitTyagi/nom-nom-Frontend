ac2722cab961806b5f625538d8349098bbff7801
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-03-17 04:26:26 +0530

    route fix

--- gateways/authGateway.ts
+++ gateways/authGateway.ts
@@ -1,6 +1,6 @@
 import { userType } from "@/utils/store";
 import axiosinstance from "../utils/axiosinstance";
-const baseURL = "/api/v1/user/";
+const baseURL = "/api/v1/user";
 import { setToken } from "@/utils/cookie";
 
 export async function login(payload: userType) {
